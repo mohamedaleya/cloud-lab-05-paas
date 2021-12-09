cbf5880d6010206314a415d13e8d38dde3c99fa6
Mohamed Aleya <gamingaleya@gmail.com>
2021-12-09 17:23:52 +0100

    Update

---
+++ data/seeds/001_first-seed.js
@@ -0,0 +1,24 @@
+const profiles = [
+  {
+    name: "Imen",
+  },
+  {
+    name: "Aymen",
+  },
+  {
+    name: "Feres",
+  },
+  {
+    name: "Nadia",
+  },
+  {
+    name: "Mohamed",
+  },
+];
+exports.seed = function (knex) {
+  return knex("profiles")
+    .del()
+    .then(() => {
+      return knex("profiles").insert(profiles);
+    });
+};
